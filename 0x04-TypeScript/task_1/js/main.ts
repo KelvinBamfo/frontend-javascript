a583ec0217a4d75d60afc4eafd11b71a93c9ef11
Kelvin Bamfo <kelvinappiah765@gmail.com>
2025-09-16 23:05:24 +0200

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,7 +18,7 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
-
+// Task 2: Director interface extending Teacher
 interface Director extends Teacher {
   numberOfReports: number;
 }
@@ -33,13 +33,54 @@ const director1: Director = {
 
 console.log(director1);
 
+// Task 3: printTeacher function and interface
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+// Named function with destructuring and exact return format
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+  return `${firstName}. ${lastName}`;
+}
 
 // Example usage
-console.log(printTeacher("Ernest", "Arhin"));
+console.log(printTeacher({ firstName: "Ernest", lastName: "Arhin" })); // Ernest. Arhin
+console.log(printTeacher("Ernest", "Arhin")); // E. Arhin
+
+// Task 4: Writing a class
+
+// Interface for constructor
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Interface for class methods
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementation
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student1 = new StudentClass({ firstName: 'Kelvin', lastName: 'Bamfo' });
+console.log(student1.workOnHomework()); // Currently working
+console.log(student1.displayName());    // Kelvin
