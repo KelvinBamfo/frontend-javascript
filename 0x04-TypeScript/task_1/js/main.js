6ef9e9f4a796cab1138d7ea9407f05051651ebeb
Kelvin Bamfo <kelvinappiah765@gmail.com>
2025-09-16 22:10:17 +0200

    Task 3: printTeacher function and interface

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,16 @@
+var teacher3 = {
+    firstName: 'Ernest',
+    lastName: 'Arhin',
+    fullTimeEmployee: false,
+    location: 'Assin Fosu',
+    contract: false
+};
+console.log(teacher3);
+var director1 = {
+    firstName: 'Ernest',
+    lastName: 'Arhin',
+    location: 'Assin Fosu',
+    fullTimeEmployee: true,
+    numberOfReports: 15
+};
+console.log(director1);
