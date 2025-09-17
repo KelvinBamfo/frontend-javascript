509c1d83e0cc571307c743c7a2493d04eaa90590
Kelvin Bamfo <kelvinappiah765@gmail.com>
2025-09-17 19:54:30 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -56,7 +56,7 @@ interface StudentClassInterface {
   displayName(): string;
 }
 
-class StudentClass implements StudentClassInterface {
+class StudentClass {
   firstName: string;
   lastName: string;
 
